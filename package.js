0239d757d3d4caf49dd7038c8147f950ac1b8583
Patrick Schubert <patrick.schubert1@gmail.com>
2024-07-01 18:29:14 +0200

    support for Meteor 3 version

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'orderlion:cdn',
-  version: '1.3.0',
+  version: '1.4.0',
   summary: 'Serve Meteor content from a CDN',
   git: 'https://github.com/orderlion/meteor-cdn',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('2.8.2');
+  api.versionsFrom(['2.8.2', '3.0-rc.4']);
   api.export('CDN','server');
   api.export('CDN','client');
   api.use('ecmascript');
   api.use('webapp','server');
-  api.use('browser-policy', {weak: true});
+  api.use('browser-policy', { weak: true });
   api.mainModule('lib/client.js','client');
   api.mainModule('lib/server.js','server');
 });
@@ -29,5 +29,3 @@ Package.onUse(function(api) {
 //   api.addFiles('tests/fixtures/icomoon.woff', 'client', {isAsset: true});
 //   api.addFiles('tests/fixtures/icomoon.otf', 'client', {isAsset: true});
 // });
-
-
